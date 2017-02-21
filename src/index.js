fc2bdf465d63295137f6d4cdcc6a600d821d5a95
Tung <me@tungv.com>
2017-02-21 15:35:46 +0700

    WIP

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
+/* @flow */
 import { config } from 'dotenv';
 import { json } from 'micro';
+
+type ActionType = {
+  type: string,
+  payload: any,
+}
 
 if (process.env.NODE_ENV !== 'production') {
   config();
 }
 
 console.log('process.env.REDIS_ENDPOINT', process.env.REDIS_ENDPOINT);
+
+const appendAction = (action: ActionType) => {
+  
+}
 
 export default async (req, res) => {
   const { url, method } = req;
